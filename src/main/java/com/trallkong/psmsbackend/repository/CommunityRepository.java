fcf29340fb7c680061e593f63e1fc68abc7c1237
Trallkongn <trallkongn@petalmail.com>
2025-12-21 17:59:02 +0800

    feat(community): 删除社区管理相关代码
    移除了社区实体、控制器、服务和仓库类，不再支持社区相关的增删改查功能。

--- src/main/java/com/trallkong/psmsbackend/repository/CommunityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.trallkong.psmsbackend.repository;
-
-import com.trallkong.psmsbackend.entity.Community;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommunityRepository extends JpaRepository<Community, Integer> {
-}
